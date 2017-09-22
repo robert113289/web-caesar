7790ab2df988ec1db191dfa3496ba11175df183a
Robert Kirkum <robert113289@gmail.com>
2017-09-22 08:52:34 -0500

    rendered form after encryption

--- main.py
+++ main.py
@@ -8,28 +8,31 @@ app.config['DEBUG'] = True
 form = """<html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea{
+            }}
+            textarea {{
                 margin:10px 0;
                 width:540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <!-- create your form here -->
         <form method='POST'>
             <label for="rotate-by">Rotate by:
-                <input type="text" name="rot" value='0'/>
+                <input type="number" min="0" name="rot" value='0'/>
             </label>
-            <input type="text" name="text"/>
+            <textarea name="text">{0}</textarea>
+
+
+
             <input type="submit" value='Submit Query' />
             
 
@@ -41,12 +44,12 @@ form = """<html>
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
     text = request.form['text']
-    return rotate_string(text,rot)
+    return form.format(rotate_string(text,rot))
 
 app.run()
